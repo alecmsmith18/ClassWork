d33f29e87401351e7a251673ebe428804f48c5b4
Alec M. Smith <X00339486@wcc.dom>
2016-09-30 09:50:21 -0500

    Class Work

---
+++ Bad Local.py
@@ -0,0 +1,8 @@
+def main ():
+    get_name ()
+    print("Hellow", name)
+
+def get_name():
+    name = input ("Enter your name: ")
+
+main()
---
+++ u.py
@@ -0,0 +1,16 @@
+def mult_2(num1, num2):
+    the_result = num1 * num2
+    print(the_result)
+
+def show_double(number):
+    number = 20
+    result = number * 2
+    print(result)
+
+def main():
+    value = float (input('enter a number'))
+    value2 = float (input ('enter a 2nd number'))
+    show_double(value)
+    mult_2(value, num2)
+
+main()
