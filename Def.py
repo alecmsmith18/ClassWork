c4ab354289f0a2f4f19d156f6fabebdb40eec5f0
Alec M. Smith <X00339486@wcc.dom>
2016-09-30 08:52:58 -0500

    Work in Class

---
+++ Def.py
@@ -0,0 +1,13 @@
+def check_the_weather():
+    print("Its nice out!")
+
+    def message():
+        print("I am Arthur")
+        print("King of the Britions")
+        check_the_weather()
+
+message()
+
+print("That's all!")
+print("hi")
+print("here")
---
+++ Functionality.py
@@ -0,0 +1,16 @@
+def step1():
+    print("Taking a shower")
+
+def step2():
+    print("Brushing teeth!")
+
+def step3():
+    print("Eating Breakfeast!")
+
+def main():
+    print("Getting ready for the day!")
+    step1()
+    step2()
+    step3()
+
+main()
